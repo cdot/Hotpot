66e09adc09ba4f6cbbb2e0eff2464f614f863da2
Crawford Currie <git@c-dot.co.uk>
2016-04-12 20:13:28 +0100

    Browser

--- browser/js/browser.js
+++ browser/js/browser.js
@@ -9,12 +9,12 @@
     };
 
     var ping = function() {
-        $.getJSON("http://192.168.1.15:13196/", function(data) {
+        $.getJSON("192.168.1.15:13196", function(data) {
             $("#time").text(data.time);
             populate("HW", data);
             populate("CH", data);
-            setTimeout(ping, 1000);
         });
+        setTimeout(ping, 1000);
     };
 
     $(document).ready(function() {
