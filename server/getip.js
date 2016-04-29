0dfb165c6b7267dac38c7a9f9ce5cdc27d2794ce
Crawford Currie <git@c-dot.co.uk>
2016-04-29 11:56:18 +0100

    Cleanup getip

--- server/getip.js
+++ server/getip.js
@@ -1,98 +1,114 @@
-// Determine the current internet IP address from a netgear router, then
-// upload it to http://c-dot.co.uk/hotpot.ip
-
 /**
-Config format
-{
-    router: {
-        host: "192.168.1.1", // usually
-        port: 80,
-        path: "/RST_status.htm",
-        headers: {
-            authorization: "Basic "
-                + new Buffer("admin:password").toString("base64")
-        }
-    },
-    ftp: {
-        host: "ftp.host to use for uploading",
-        user: "ftp.user",
-        pass: "ftp.pass"
-        path: "path to file to create on ftp server"
-    },
-    http: {
-        host: "host where ftp uploads are seen",
-        port: "80",
-        path: "/path to the same file as in the ftp uploads"
-    }
-}
-*/
+ * Stand-alone program to:
+ * Scrape the current public IP address of our Netgear router (DHCP allocated)
+ * Compare it with the IP address stored in a web-accessible file
+ * Update that file if the address is different
+ */
+
+//Config format
+//{
+//    debug: false, // or true
+//    // Assumes router has a page (in this case, RST_status.htm) that
+//    // shows the current IP address in a table that we can scrape it
+//    // from.
+//    router: {
+//        host: "192.168.1.1", // usually
+//        port: 80,
+//        path: "/RST_status.htm",
+//        headers: {
+//            authorization: "Basic "
+//                + new Buffer("admin:password").toString("base64")
+//        }
+//    },
+//    // Webspace where the IP file is stored
+//    http: {
+//        host: "host where ftp uploads are seen",
+//        port: "80",
+//        path: "/path to the same file as in the ftp uploads"
+//    }
+//    // Details of FTP server used to manage the webspace
+//    ftp: {
+//        host: "ftp.host to use for uploading",
+//        user: "ftp.user",
+//        pass: "ftp.pass"
+//        path: "path to file to create on ftp server"
+//    }
+//}
 
 var http = require("http");
 var JSFtp = require("jsftp");
 const Fs = require("fs");
 
-const search_re = /<td.*?>IP Address.*?<td.*?>([0-9.]+)</g;
+// Regex used to scrape the report from the router
+const search_re = /IP Address.*?(\d+\.\d+\.\d+\.\d+)/g;
 
 var config;
 eval("config=" + Fs.readFileSync("./getip.config"));
 
-function update_address(old_addr, new_addr) {
+// Update the IP address in the file, if it has changed
+function update_address(new_addr) {
     "use strict";
-    if (old_addr === new_addr) {
-        console.log("IP address unchanged");
-        return;
-    }
 
-    console.log("Updating IP address");
+    console.TRACE("Updating IP address");
     var Ftp = new JSFtp(config.ftp);
 
-    Ftp.on("jsftp_debug", function(eventType, data) {
-        console.log("DEBUG: ", eventType);
-        console.log(JSON.stringify(data, null, 2));
-    });
+    if (config.ftp.debugEnable) {
+        Ftp.on("jsftp_debug", function(eventType, data) {
+            console.TRACE("DEBUG: ", eventType);
+            console.TRACE(JSON.stringify(data, null, 2));
+        });
+    }
 
-    console.log("Push up new IP address " + new_addr);
+    console.TRACE("Push up new IP address " + new_addr);
     Ftp.put(new Buffer(new_addr), config.ftp.path,
             function(hadErr) {
                 if (hadErr)
-                    console.error("Had an error" + hadErr);
+                    console.TRACE("Had an error" + hadErr);
                 else
-                    console.log("hotpot.ip updated");
+                    console.TRACE("hotpot.ip updated");
+                Ftp.raw.quit();
             });
 }    
 
-function check_existing_address(new_addr) {
+// Get the content of the existing IP address file from the webspace
+// callback(ip_addr, err) err is non-null if there's a problem
+function get_existing_address(callback) {
     "use strict";
-    var old_addr = "";
-    console.log("Getting old IP address from website");
+    var addr = "";
     http.get(config.http,
              function(res) {
                  res.on("data", function(chunk) {
-                     old_addr += chunk;
+                     addr += chunk;
                  });
                  res.on("end", function() {
-                     update_address(old_addr, new_addr);
+                     callback(addr);
                  });
              })
         .on("error", function(err) {
-            console.log("Cannot get existing address: " + err);
-            update_address("unknown", new_addr);
+            callback(null, err);
         });
 }
 
-function publish_ip_address(output) {
+// Scrape the IP address from the router report
+// callback(ip_addr, err) err is non-null if there's a problem
+function scrape_ip_address(output, callback) {
     "use strict";
     output = output.replace(/\s+/g, " ");
+    console.TRACE("Scraping IP address")
     var match = search_re.exec(output);
-    if (match)
-        check_existing_address(match[1]);
-    else
-        console.error("Router output strange: No match for " + search_re + " in " + output);
+    if (match) {
+        callback(match[1]);
+    } else {
+        callback(null, "Router output strange: No match for "
+                 + search_re + " in " + output);
+    }
 }
 
-function get_ip_from_router() {
+// Get the current IP address from the router, and call callback with it.
+// callback(ip_addr, err) err is non-null if there's a problem
+function get_ip_from_router(callback) {
     "use strict";
-    console.log("Getting IP address from router");
+    console.TRACE("Getting IP address from router");
     http.get(
         config.router,
         function(res) {
@@ -102,16 +118,44 @@ function get_ip_from_router() {
             });
 
             res.on("end", function() {
-                if (/"is managing this device"/.exec(output) !== null)
-                    console.error("Cannot talk to router: Another manager is logged in");
+                //console.TRACE("Response received from router " + output);
+                if (/Authorization failed/.exec(output) !== null)
+                    console.TRACE(
+                        "Cannot talk to router: Authorisation failed");
+                else if (/is managing this device/.exec(output) !== null)
+                    console.TRACE(
+                        "Cannot talk to router: Another manager is logged in");
                 else
-                    publish_ip_address(output);
+                    scrape_ip_address(output, callback);
             });
         })
 
         .on("error", function(err) {
-            console.log("Cannot talk to router: " + err);
+            console.TRACE("Cannot talk to router: " + err);
         });
 }
 
-get_ip_from_router();
+if (config.debug)
+    console.TRACE = console.log;
+else
+    console.TRACE = function() {};
+
+get_ip_from_router(
+    function(new_addr, scraperr) {
+        if (scraperr)
+            console.TRACE("Cannot scrape router output: " + err);
+        else
+            console.TRACE("Router IP address " + new_addr);
+            get_existing_address(function(old_addr, geterr) {
+                console.TRACE("Existing IP address " + old_addr);
+                if (geterr)
+                    console.TRACE(
+                        "Cannot get existing address: " + geterr);
+                // force update
+                // old_addr="ignored";
+                if (old_addr !== new_addr)
+                    update_address(new_addr);
+                else
+                    console.TRACE("No need to update");
+            });
+    });
