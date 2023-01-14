1a24fa4c1cfa77a18f6a21b7089a68b4d151a0a9
cdot <github@c-dot.co.uk>
2023-01-14 15:58:37 +0000

    node-getopt no longer used

--- test/server/testDS18x20.js
+++ test/server/testDS18x20.js
@@ -7,22 +7,36 @@ requirejs.config({
 	baseUrl: __dirname + "/../.."
 });
 
-requirejs(["node-getopt", "js/common/Utils", "js/server/DS18x20"], function(Getopt, Utils, DS18x20) {
+requirejs(["posix-getopt", "js/common/Utils", "js/server/DS18x20"], function(getopt, Utils, DS18x20) {
 
-const ASYNC = true;
+  const ASYNC = true;
 
-	let getopt = new Getopt([
-		[ "h", "help", "Show this help" ],
-		[ "p", "poll=ARG", "Poll the device(s) every ARG seconds" ]
-	])
-	.setHelp(`Usage: node ${process.argv[1]} [OPTION] <sensor>\n`
-			 + "With no options, get the value of all sensors\n\n"
-			 + "[[OPTIONS]]")
-		.bindHelp()
-		.parseSystem();
+  const go_parser = new getopt.BasicParser(
+    "h(help)p:(poll)",
+    process.argv);
 
-	let cliopt = getopt.options;
-	let freq = cliopt.poll ? parseFloat(cliopt.poll) : 2;
+	const DESCRIPTION = [
+    "DESCRIPTION",
+    "Read DS18x20 one-wire temperature sensors",
+	  "USAGE",
+    `node ${process.argv[1]} [OPTION] <sensor>`,
+		"With no options, get the value of all sensors",
+    "OPTIONS",
+		"\t-h, --help -Show this help",
+		"\t-p, --poll=ARG - Poll the device(s) every ARG seconds"
+  ].join("\n");
+
+	const cliopt = {};
+  let option;
+  while ((option = go_parser.getopt())) {
+    switch (option.option) {
+    case 'p': cliopt.poll = option.optarg; break;
+    case 'd': cliopt.debug = true; break;
+    default: console.log(DESCRIPTION); process.exit(0);
+	  }
+  }
+	let id = process.argv[go_parser.optind()];
+  let freq = cliopt.poll ? parseFloat(cliopt.poll) : 2;
 
 	// Number of ms since last-known-good sample
 	let longestWait = [];
@@ -32,48 +46,47 @@ const ASYNC = true;
 		// This could (might but probably won't) result in asynchronous reads from the
 		// 1-wire bus.
 		let promise;
-if (ASYNC) {
-		promise = Promise.all(sensors.map(sensor => {
-			sensor.getTemperature()
-			.then(t => {
-				let now = Date.now();
-				let diff = (now - lastKnownGood[sensor.id]) / 1000;
-				console.log(`${sensor.id}: ${t} ${diff}`);
-				lastKnownGood[sensor.id] = now;
-			 })
-			.catch(() => {
-				let wait = (Date.now() - lastKnownGood[sensor.id]) / 1000;
-				if (wait > longestWait[sensor.id])
-					longestWait[sensor.id] = wait;
-				console.error(`Nothing from ${sensor.id} for ${wait} (${longestWait[sensor.id]})`);
-	 		});
-		}));
-} else {
-		promise = Promise.resolve();
-		for (let i in sensors) {
-			let sensor = sensors[i];
-			promise = promise.then(() => sensor.getTemperature())
-			.then(t => {
-				let now = Date.now();
-				let diff = (now - lastKnownGood[sensor.id]) / 1000;
-				console.log(`${sensor.id}: ${t} ${diff}`);
-				lastKnownGood[sensor.id] = now;
-			 })
-			.catch(() => {
-				let wait = (Date.now() - lastKnownGood[sensor.id]) / 1000;
-				if (wait > longestWait[sensor.id])
-					longestWait[sensor.id] = wait;
-				console.error(`Nothing from ${sensor.id} for ${wait} (${longestWait[sensor.id]})`);
-	 		});
-		}
-}
+    if (ASYNC) {
+		  promise = Promise.all(sensors.map(sensor => {
+			  sensor.getTemperature()
+			  .then(t => {
+				  let now = Date.now();
+				  let diff = (now - lastKnownGood[sensor.id]) / 1000;
+				  console.log(`${sensor.id}: ${t} ${diff}`);
+				  lastKnownGood[sensor.id] = now;
+			  })
+			  .catch(() => {
+				  let wait = (Date.now() - lastKnownGood[sensor.id]) / 1000;
+				  if (wait > longestWait[sensor.id])
+					  longestWait[sensor.id] = wait;
+				  console.error(`Nothing from ${sensor.id} for ${wait} (${longestWait[sensor.id]})`);
+	 		  });
+		  }));
+    } else {
+		  promise = Promise.resolve();
+		  for (let i in sensors) {
+			  let sensor = sensors[i];
+			  promise = promise.then(() => sensor.getTemperature())
+			  .then(t => {
+				  let now = Date.now();
+				  let diff = (now - lastKnownGood[sensor.id]) / 1000;
+				  console.log(`${sensor.id}: ${t} ${diff}`);
+				  lastKnownGood[sensor.id] = now;
+			  })
+			  .catch(() => {
+				  let wait = (Date.now() - lastKnownGood[sensor.id]) / 1000;
+				  if (wait > longestWait[sensor.id])
+					  longestWait[sensor.id] = wait;
+				  console.error(`Nothing from ${sensor.id} for ${wait} (${longestWait[sensor.id]})`);
+	 		  });
+		  }
+    }
 		promise.finally(() => {
 			if (freq > 0)
 				Utils.startTimer("freq", () => poll(sensors), freq * 1000);
 		});
 	}
 
-	let id = getopt.argv[0];
 	((typeof id === "undefined") ? DS18x20.list() : Promise.resolve([id]))
 	.then(ids => {
 		console.log("Sensors ", ids);
